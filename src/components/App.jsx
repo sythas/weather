052266727f6f72f93bf00edf7757ba72716b954d
Jack Smith <jack@sythas.com>
2021-04-13 14:21:40 -0500

    Added hooks to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,30 @@
 import Stack from './Stack'
 import Today from './Today'
 import Week from './Week'
+import Place from './Place'
 import ChangeTheme from './ChangeTheme'
 import { useContext} from 'react'
 import { ThemeContext } from './Theme'
 import Divider from './Divider'
+import useLocation from '../hooks/useLocation'
+import useForecast from '../hooks/useForecast'
 
 function App() {
   const { theme } = useContext(ThemeContext)
+  const [location, locationError] = useLocation()
+  const [forecast, forecastError] = useForecast(location)
+
+  console.log(forecast)
+
+  locationError && console.log("location error", locationError)
+  forecastError && console.log("location error", forecastError)
 
   return (
     <Stack style={{...theme, height: '100%', padding: '1rem'}}>
       <ChangeTheme />
-      <Today />
+      <Place place={forecast?.place} />
+      <Divider />
+      <Today forecast={forecast?.periods[0]} />
       <Divider />
       <Week />
     </Stack>
